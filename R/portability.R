be44f2398eeb29e019d0a1b6444cc9ff38d62a25
HughParsonage <hugh.parsonage@gmail.com>
2016-10-15 11:23:45 +1100

    Ensure portability for TRAVIS

---
+++ R/portability.R
@@ -0,0 +1,23 @@
+
+portability <- function(){
+  requiredPackages <- c("assertthat", "betareg", "broom", "car", "chron", "codetools", 
+                        "colorspace", "CostOverrunsData", "crayon", "curl", "data.table", 
+                        "DBI", "devtools", "digest", "dplyr", "dtplyr", "evaluate", "flexmix", 
+                        "foreach", "foreign", "formatR", "Formula", "ggplot2", "glmnet", 
+                        "grattan", "gtable", "iterators", "knitr", "lattice", "lazyeval", 
+                        "lme4", "lmtest", "logistf", "magrittr", "MASS", "Matrix", "MatrixModels", 
+                        "memoise", "mfx", "mgcv", "mice", "minqa", "mnormt", "modeltools", 
+                        "munsell", "nlme", "nloptr", "nnet", "pbkrtest", "plyr", "psych", 
+                        "quantreg", "R6", "Rcpp", "reshape2", "rpart", "rsconnect", "rstudioapi", 
+                        "sandwich", "scales", "SparseM", "stringi", "stringr", "survival", 
+                        "testthat", "tibble", "tidyr", "withr", "zoo")
+  
+  PackagesRequiredByUser <- setdiff(requiredPackages, installed.packages())
+  
+  if (Sys.getenv("CI") == "true" && Sys.getenv("TRAVIS") == "true") {
+    if (length(PackagesRequiredByUser) != 0){
+      install.packages(requiredPackages)
+      devtools::install_github("hughparsonage/grattan")
+    }
+  }
+}
