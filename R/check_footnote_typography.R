5b7aefacdcc5907ec445f13e0d681c8395ad314f
HughParsonage <hugh.parsonage@gmail.com>
2016-10-21 22:17:07 +1100

    Rm space before footnote

--- R/check_footnote_typography.R
+++ R/check_footnote_typography.R
@@ -18,13 +18,25 @@ check_footnote_typography <- function(filename, ignore.lines = NULL){
     if (is.infinite(footnote_closes_at))
       break
     split_line_after_footnote <- strsplit(gsub("^.*footnote", "", line, perl = TRUE), split = "")[[1]]
-    if (length(split_line_after_footnote) > footnote_closes_at && split_line_after_footnote[footnote_closes_at + 1] == "."){
+    if (length(split_line_after_footnote) > footnote_closes_at && split_line_after_footnote[footnote_closes_at + 1] %in% c(".", ",")){
       cat(paste0(split_line_after_footnote,
                  collapse = ""),
           "\n")
       stop("Full stop after footnotemark.")
     }
   }
+  message("No full stops after footnotemarks")
+  rm(line)
+  
+  for (line in lines){
+    if (grepl(" \\footnote", line, fixed = TRUE)){
+      cat(line)
+      stop("Space before footnote.")
+    }
+  }
+  message("No space before footnote marks")
+  
+  # Check spaces before footnotes
 }
 
 
